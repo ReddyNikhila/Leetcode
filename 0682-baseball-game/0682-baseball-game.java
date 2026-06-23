fc44f23ce71f3b135144da7f78798201657a4507
Reddy Nikhila <23p31a0547@acet.ac.in>
2026-06-23 22:34:32 +0530

    Time: 2 ms (95.64%), Space: 43.6 MB (29.43%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calPoints(String[] operations) {
+         List<Integer> record = new ArrayList<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+                record.remove(record.size() - 1);
+            }
+
+            else if (op.equals("D")) {
+                record.add(2 * record.get(record.size() - 1));
+            }
+
+            else if (op.equals("+")) {
+                int n = record.size();
+                record.add(record.get(n - 1) + record.get(n - 2));
+            }
+
+            else {
+                record.add(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+        for (int score : record) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
