5459dbd3011ff9261d92175d957e36a580b71d77
Reddy Nikhila <23p31a0547@acet.ac.in>
2026-08-17 11:20:49 +0530

    Time: 107 ms (35.66%), Space: 141.9 MB (65.19%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,28 +1,31 @@
 class NumMatrix {
-    private int[][] prefixSum;
+
+    int[][] prefix;
 
     public NumMatrix(int[][] matrix) {
+
         int m = matrix.length;
         int n = matrix[0].length;
-        
-        // Pad the DP table size by 1 to simplify boundary logic
-        prefixSum = new int[m + 1][n + 1];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                prefixSum[i + 1][j + 1] = matrix[i][j] 
-                                        + prefixSum[i][j + 1] 
-                                        + prefixSum[i + 1][j] 
-                                        - prefixSum[i][j];
+
+        prefix = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                prefix[i][j] =
+                    matrix[i - 1][j - 1]
+                    + prefix[i - 1][j]
+                    + prefix[i][j - 1]
+                    - prefix[i - 1][j - 1];
             }
         }
     }
-    
+
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        // Inclusion-Exclusion Principle formula
-        return prefixSum[row2 + 1][col2 + 1] 
-             - prefixSum[row1][col2 + 1] 
-             - prefixSum[row2 + 1][col1] 
-             + prefixSum[row1][col1];
+
+        return prefix[row2 + 1][col2 + 1]
+             - prefix[row1][col2 + 1]
+             - prefix[row2 + 1][col1]
+             + prefix[row1][col1];
     }
 }
